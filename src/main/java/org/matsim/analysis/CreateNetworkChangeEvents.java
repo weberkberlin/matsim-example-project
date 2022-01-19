62705a803386f19f0571ae5847ff32705d7aced3
tschlenther <schlenther@vsp.tu-berlin.de>
2022-01-19 13:01:24 +0100

    first version of analysis script. TODO: generate random coords

---
+++ src/main/java/org/matsim/analysis/CreateNetworkChangeEvents.java
@@ -0,0 +1,122 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.analysis;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.network.NetworkChangeEvent;
+import org.matsim.core.network.NetworkChangeEvent.ChangeType;
+import org.matsim.core.network.NetworkChangeEvent.ChangeValue;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.network.io.NetworkChangeEventsWriter;
+import org.matsim.core.trafficmonitoring.TravelTimeCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+/**
+ * This class is copied from matsim-code-examples and shows how to generate Network Change Events based on the speeds measured in an existing simulation.
+ */
+public class CreateNetworkChangeEvents{
+
+	//settings
+	private static final int ENDTIME = 36 * 3600;
+	private static final int TIMESTEP = 15 * 60;
+
+	//input files
+	private static final String NETWORKFILE = "";
+	private static final String SIMULATION_EVENTS_FILE = "";
+	static final String OUTPUT_CHANGE_EVENTS_FILE = "";
+	private static final double MINIMUMFREESPEED = 3;
+
+
+	private final String networkFile;
+	private final String simulationEvents;
+	private final String outputEvents;
+
+	CreateNetworkChangeEvents(String inputNetworkFile, String inputEventsFile, String outputFile) {
+		networkFile = inputNetworkFile;
+		simulationEvents = inputEventsFile;
+		outputEvents = outputFile;
+	}
+
+	public static void main(String[] args) {
+		CreateNetworkChangeEvents ncg = new CreateNetworkChangeEvents(NETWORKFILE, SIMULATION_EVENTS_FILE, OUTPUT_CHANGE_EVENTS_FILE);
+		ncg.run();
+	}
+
+	void run() {
+		Network network = NetworkUtils.createNetwork() ;
+		new MatsimNetworkReader(network).readFile(this.networkFile);
+		TravelTimeCalculator tcc = readEventsIntoTravelTimeCalculator( network );
+		List<NetworkChangeEvent> networkChangeEvents = createNetworkChangeEvents( network, tcc );
+		new NetworkChangeEventsWriter().write(this.outputEvents, networkChangeEvents);
+	}
+
+	private List<NetworkChangeEvent> createNetworkChangeEvents( Network network, TravelTimeCalculator tcc ) {
+		List<NetworkChangeEvent> networkChangeEvents = new ArrayList<>() ;
+
+		for (Link l : network.getLinks().values()) {
+
+//			if (l.getId().toString().startsWith("pt")) continue;
+
+			double length = l.getLength();
+			double previousTravelTime = l.getLength() / l.getFreespeed();
+
+			for (double time = 0; time < ENDTIME; time = time + TIMESTEP) {
+
+				double newTravelTime = tcc.getLinkTravelTimes().getLinkTravelTime(l, time, null, null);
+				if (newTravelTime != previousTravelTime) {
+
+					NetworkChangeEvent nce = new NetworkChangeEvent(time);
+					nce.addLink(l);
+					double newFreespeed = length / newTravelTime;
+					if (newFreespeed < MINIMUMFREESPEED) newFreespeed = MINIMUMFREESPEED;
+					ChangeValue freespeedChange = new ChangeValue(ChangeType.ABSOLUTE_IN_SI_UNITS, newFreespeed);
+					nce.setFreespeedChange(freespeedChange);
+
+					networkChangeEvents.add(nce);
+					previousTravelTime = newTravelTime;
+				}
+			}
+		}
+		return networkChangeEvents ;
+	}
+
+	private TravelTimeCalculator readEventsIntoTravelTimeCalculator( Network network ) {
+		EventsManager manager = EventsUtils.createEventsManager();
+		TravelTimeCalculator.Builder builder = new TravelTimeCalculator.Builder( network );
+		TravelTimeCalculator tcc = builder.build();
+		manager.addHandler(tcc);
+		manager.initProcessing();
+		new MatsimEventsReader(manager).readFile(this.simulationEvents);
+		manager.finishProcessing();
+		return tcc ;
+	}
+
+}
--- src/main/java/org/matsim/analysis/RunTravelTimeAnalysis.java
+++ src/main/java/org/matsim/analysis/RunTravelTimeAnalysis.java
@@ -1,28 +1,122 @@
 package org.matsim.analysis;
 
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.router.RoutingModule;
+import org.matsim.core.router.TripRouterModule;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.collections.Tuple;
+
+import java.util.List;
 
 public class RunTravelTimeAnalysis {
 
+
+    private static final String CONFIG_PATH = "";
+    private static final String NETWORK_CHANGE_EVENTS = "";
+
+    private static final String OUTPUT_EVENTS_PATH = "";
+
+    private static final String OUTPUT_DIRECTORY = "";
+    private static final Integer NUMBER_OF_AGENTS = 10_000;
+
+
+    private static final String ACT_TYPE = "analysis";
+
     public static void main(String[] args) {
-        Config config = ConfigUtils.createConfig();
 
-        //setze Input Files
+        Config config = ConfigUtils.loadConfig(CONFIG_PATH);
 
+        //create network change events
+        String outputEvents = config.controler().getOutputDirectory() + "/" +  config.controler().getRunId() + ".output_events.xml.gz";
+        new CreateNetworkChangeEvents(config.network().getInputFile(), outputEvents, NETWORK_CHANGE_EVENTS).run();
+
+        //set input paths and output dir
+        config.network().setTimeVariantNetwork(true);
+        config.network().setChangeEventsInputFile(NETWORK_CHANGE_EVENTS);
+        config.controler().setOutputDirectory(OUTPUT_DIRECTORY);
+
+
+        //register new activity type
+        PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams();
+        params.setActivityType(ACT_TYPE);
+        params.setScoringThisActivityAtAll(false);
+        config.planCalcScore().addActivityParams(params);
+
+        //load scenario
         Scenario scenario = ScenarioUtils.loadScenario(config);
 
         //modify scenario (pop building)
-        // mit https://github.com/matsim-scenarios/matsim-berlin/blob/ev/src/test/java/org/matsim/urbanEV/UrbanEVTests.java
+        overridePopulation(scenario);
 
         Controler controler = new Controler(scenario);
 
+        RoutingModule router = controler.getInjector().getInstance(RoutingModule.class);
+
         controler.run();
+    }
+
+    /**
+     * first deletes all agents from the population and then (randomly)
+     * generates new agents that have one leg each with variyng distances and relations spread over the entire network
+     * @param scenario
+     */
+    private static void overridePopulation(Scenario scenario){
+        // delete original population
+        scenario.getPopulation().getPersons().clear();
+
+        PopulationFactory factory = scenario.getPopulation().getFactory();
+
+
+        for (int i = 1; i < NUMBER_OF_AGENTS; i++) {
+
+            Person carPerson = factory.createPerson(Id.createPersonId("person_" + i + "_CAR"));
+            Person ptPerson = factory.createPerson(Id.createPersonId("person_" + i + "_PT"));
+
+            Plan carPlan = factory.createPlan();
+            Plan ptPlan = factory.createPlan();
+
+            Tuple<Coord, Coord> relation = getRandomCoordRelationInNetwork(scenario.getNetwork());
+
+            Activity originAct = factory.createActivityFromCoord(ACT_TYPE, relation.getFirst());
+            originAct.setEndTime( 8*3600 ); //TODO
+
+            Activity destinationAct = factory.createActivityFromCoord(ACT_TYPE, relation.getSecond());
+
+            carPlan.addActivity(originAct);
+            ptPlan.addActivity(originAct);
+
+            carPlan.addLeg(factory.createLeg(TransportMode.car));
+            ptPlan.addLeg(factory.createLeg(TransportMode.pt));
+
+            carPlan.addActivity(destinationAct);
+            ptPlan.addActivity(destinationAct);
+
+            carPerson.addPlan(carPlan);
+            ptPerson.addPlan(ptPlan);
+
+            scenario.getPopulation().addPerson(carPerson);
+            scenario.getPopulation().addPerson(ptPerson);
+        }
 
     }
+
+    //TODO
+    private static Tuple<Coord,Coord> getRandomCoordRelationInNetwork(Network network) {
+//        NetworkUtils.getBoundingBox()
+        //...
+        return null;
+    }
+
 
 
 }
